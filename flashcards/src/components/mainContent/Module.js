8ec7d197f44add0d685e5b6bed84a5d4c975fe35
Leana <leananeparidze.com>
2019-07-03 01:24:48 -0700

    adds desktop view for module component

--- flashcards/src/components/mainContent/Module.js
+++ flashcards/src/components/mainContent/Module.js
@@ -2,14 +2,78 @@ import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 // -------- Styled Components -------------- //
+const DesktopModule = styled.div`
+  @media (max-width: 1900px) {
+    display: flex;
+    justify-content: space-between;
+    // flex-direction: row;
+    .main-wrap {
+      width: 100%;
+    }
+  }
+  @media (max-width: 414px) {
 
+  }
+`
+const DesktopSide = styled.div`
+  min-width: 350px;
+  height: 100vh;
+  margin: 0 auto;
+  background: rgb(20, 18, 30);
+  box-sizing: border-box;
+  padding: 20px;
+  text-align: center;
+  overflow: hidden;
+ 
+
+  .links {
+    height: 100%;
+    display: block;
+    flex-direction: column;
+    justify-items: center;
+    align-content: stretch;
+    margin-top: 60%;
+  }
+
+  .logo {
+    // width: 100%;
+    display: inline-block;
+    // padding: 20px;
+    box-sizing: border-box;
+  }
+  .title {
+    font-size: 22px;
+    color: #ba112e;
+    display: inline-block;
+    padding-left: 10px;
+    box-sizing: border-box;
+  }
+  .text {
+    font-size: 20px;
+    color: white;
+    text-align: left;
+    padding: 0 0px 20px 30px;
+  }
+
+  @media (max-width: 414px) {
+    display: none;
+  }
+`
 const TopContent = styled.div`
-  padding-left: 10px;
+  padding-left: 20px;
   padding-right: 10px;
   box-sizing: border-box;
+  padding-top: 40px;
+  display: flex;
+  flex-direction: column;
   h1 {
+  }
+  .text-field {
+    justify-self: center;
+    margin: 0 auto;
   }
   input {
       width: 475px;
@@ -22,6 +86,8 @@ const TopContent = styled.div`
       border: transparent;
       box-sizing: border-box;
       position: relative;
+      justify-self: center;
+
   }
   input:focus {
       outline: none;
@@ -30,8 +96,9 @@ const TopContent = styled.div`
       color: rgb(180, 180, 180);
   }
   .icon {
-      position: absolute;
-      right: 0;
+      // position: absolute;
+      // right: 50px;
+      transform: translate(-40px, 10px);
       margin-right: 20px;
       margin-top: 10px;
   }
@@ -42,6 +109,10 @@ const TopContent = styled.div`
     box-sizing : border-box;
     h1 {
       
+    }
+    .text-field {
+      justify-self: center;
+      margin: 0;
     }
     input {
       width: 100%;
@@ -66,6 +137,7 @@ const TopContent = styled.div`
       right: 0;
       margin-right: 20px;
       margin-top: 10px;
+      transform: translate(0px, 0px);
     }
   }
 `;
@@ -74,39 +146,56 @@ const MainContent = styled.div`
     a {
         color: black;
     }
+    display: flex;
+    flex-wrap: wrap;
 
     @media (max-width: 414px) {
+      display: block;
+      flex-wrap: none;
     }
 `;
 
 const Card = styled.div`
   background: rgb(214, 223, 231);
-  margin: 20px 10px 20px 10px;
+  margin: 20px 70px 20px 70px;
   border-radius: 5px;
-  padding: 10px 0 10px 0;
-  max-width: 475px;
+  // padding: 10px 0 10px 0;
+  width: 320px;
+  height: 160px;
+  // padding: 10px;
+  // display: flex;
+  // justify-content: space-between;
+  // align-content: center;
+
+
+
   :hover {
-    transform: scale(1.1);
+    transform: scale(1.01);
   }
   .first-row {
+      height: 100%;
       display: flex;
       justify-content: space-between;
-      align-items: center;
+      align-content: middle;
+      margin: 0 auto;
       padding: 0 10px 15px 10px;
       font-weight: bold;
-      p {
-          margin: 0;
-          font-size: 20px;
-      }
+      top: 50%;
+
       .number {
           font-size: 16px;
-          padding-top: 3px;
+          padding-top: 0px;
           color: #ba112e;
+          margin-top: 70px;
+      }
+      .text {
+        margin-top: 70px;
       }
   }
   .p {
       padding: 10px;
       margin: 0px;
+      margin-top: 20%;
   }
 
   @media (max-width: 414px) {
@@ -114,19 +203,24 @@ const Card = styled.div`
     margin: 20px 10px 20px 10px;
     border-radius: 5px;
     padding: 10px 0 10px 0;
+    width: auto;
+    height: 100px;
+
+
     .first-row {
       display: flex;
       justify-content: space-between;
-      align-items: center;
+      align-content: center;
+      margin: 0 auto;
       padding: 0 10px 15px 10px;
-      p {
-        margin: 0;
-        font-size: 20px;
+
+      .text {
+        margin-top: 40px;
       }
       .number {
         font-size: 16px;
-        padding-top: 3px;
         color: #BA112E;
+        margin-top: 40px;
       }
     }
     .p {
@@ -312,10 +406,26 @@ class Module extends Component {
     render() {
         if (this.state.flashcards.length > 0 && this.state.section.length > 0) {
             return (
-                <div>
+              <DesktopModule>
+                <DesktopSide>
+                  <div>
+                    <img className="logo" src={logo} alt="logo"/>
+                    <div className="title">Lambda Flash Cards</div>
+                  </div>
+                  <div className="links">
+                    <Link to="/" style={{ textDecoration: 'none' }}><div className="text" >Home</div></Link>
+                    <Link to="/android_development" style={{ textDecoration: 'none' }}><div className="text" >Android Development</div></Link>
+                    <Link to="/full_stack" style={{ textDecoration: 'none' }}><div className="text" >Full Stack Web</div></Link>
+                    <Link to="/data_science" style={{ textDecoration: 'none' }}><div className="text" >Data Science</div></Link>
+                    <Link to="/ux_design" style={{ textDecoration: 'none' }}><div className="text" >User Experience Design</div></Link>
+                    <Link to="/ios_development" style={{ textDecoration: 'none' }}><div className="text" >iOS Development</div></Link>
+                    <Link to="/computer_science" style={{ textDecoration: 'none' }}><div className="text" >Computer Science</div></Link>
+                  </div>
+                </DesktopSide>
+                <div className="main-wrap">
                     <TopContent>
                         <h1>{this.props.module}</h1>
-                        <div>
+                        <div className="text-field">
                             <input
                                 type="text"
                                 className="input"
@@ -361,7 +471,7 @@ class Module extends Component {
                             >
                                 <Card onClick={() => this.handleClick(item)}>
                                     <div className="first-row">
-                                        <p>{item}</p>
+                                        <p className="text">{item}</p>
                                         <p className="number">
                                             {
                                                 this.state.flashcards[index]
@@ -370,15 +480,13 @@ class Module extends Component {
                                             Flashcards
                                         </p>
                                     </div>
-                                    <p className="p">
-                                       
-                                    </p>
                                 </Card>
                             </Link>
                         ))}
                         
                     </MainContent>
                 </div>
+            </DesktopModule>
             );
         } else {
             return (
