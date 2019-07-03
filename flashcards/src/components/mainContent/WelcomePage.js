7438d34e727b6af731139639ee03ba3505bd1ffd
droneal11015 <41247530+droneal11015@users.noreply.github.com>
2019-07-02 22:33:44 -0400

    working on flex properties to display sections as boxes

--- flashcards/src/components/mainContent/WelcomePage.js
+++ flashcards/src/components/mainContent/WelcomePage.js
@@ -25,6 +25,19 @@ const MainContent = styled.div`
 padding-top: 30px;
 text-align: center;
 
+.boxesall {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+.boxestop {
+  display: flex;
+  justify-content: space-evenly;
+
+}
+.boxesbottom {
+  display: flex;
+  justify-content: space-evenly;
 
 }
 .box {
